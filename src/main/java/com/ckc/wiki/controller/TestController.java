de825d9400c47bee9bf55c0f7b168db1a2499174
chuikokching <455344883@qq.com>
2021-11-12 11:10:10 +0100

    2-7 Http测试类开发

--- src/main/java/com/ckc/wiki/controller/TestController.java
+++ src/main/java/com/ckc/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.ckc.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /*@Controller 返回页面*/
@@ -23,6 +24,13 @@ public class TestController {
     @GetMapping("/hello")
     public String hello()
     {
+
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name)
+    {
+        return "Hello World! Post "+name;
     }
 }
