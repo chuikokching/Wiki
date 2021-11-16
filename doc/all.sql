e8e6585925ff8f330c9683afff991dff20d83dfe
chuikokching <455344883@qq.com>
2021-11-16 15:52:17 +0100

    3-5 集成mybatis,生成demo表代码

--- doc/all.sql
+++ doc/all.sql
@@ -6,6 +6,17 @@ create table `test`(
   primary key(`id`)
 ) engine =innodb default charset = utf8mb4 comment '测试';
 
-select * from ckc_wiki_project.test;
+select * from ckc_wiki_projectdev.test;
 
 insert into `test` values (1, "chuikokching","123456");
+
+drop table if exists `demo`;
+create table `demo`(
+  `id` bigint not null comment 'ID',
+  `name` varchar(50) comment '名称',
+  primary key(`id`)
+) engine =innodb default charset = utf8mb4 comment '测试';
+
+insert into `demo` values (1, "chuikokching");
+
+select * from ckc_wiki_projectdev.demo;
