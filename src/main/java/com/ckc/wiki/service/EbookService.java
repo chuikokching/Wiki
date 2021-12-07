2ab0934b500fa85e2f915f2dbceb28ff52d44488
chuikokching <455344883@qq.com>
2021-12-07 13:53:20 +0100

    3-8 制作CopyUtil封装BeanUtils

--- src/main/java/com/ckc/wiki/service/EbookService.java
+++ src/main/java/com/ckc/wiki/service/EbookService.java
@@ -5,10 +5,10 @@ import com.ckc.wiki.domain.EbookExample;
 import com.ckc.wiki.mapper.EbookMapper;
 import com.ckc.wiki.req.EbookReq;
 import com.ckc.wiki.resp.EbookResp;
+import com.ckc.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,20 +25,24 @@ public class EbookService {
         criteria.andNameLike("%"+req.getName()+"%");
         //criteria.andDescriptionLike("%"+req.getName()+"%");
 
-
         List<Ebook> ebookList  = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            /*可以选择性返回字段给用户*/
-            //ebookResp.setId(ebook.getId());
-            ebookResp.setName(ebook.getName());
 
-            /*对象直接复制*/
-            //BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+//            //EbookResp ebookResp = new EbookResp();
+//            /*可以选择性返回字段给用户*/
+//            //ebookResp.setId(ebook.getId());
+//            //ebookResp.setName(ebook.getName());
+//
+//            /*对象直接复制*/
+//            //BeanUtils.copyProperties(ebook, ebookResp);
+//            EbookResp copy = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(copy);
+//        }
+
+        //List<EbookResp> list = CopyUtil.copyList(ebookList,EbookResp.class);
+
+        return CopyUtil.copyList(ebookList,EbookResp.class);
     }
 }
