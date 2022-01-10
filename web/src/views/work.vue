0a0e2a681a3c36368c93a341e5a1362515322048
chuikokching <455344883@qq.com>
2022-01-10 18:20:33 +0100

    5-9-extra 添加work个人作品模块页面

---
+++ web/src/views/work.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <the-header></the-header>
+    <the-sidebar></the-sidebar>
+    <div class="work">
+        <h1>This is a work page.</h1>
+    </div>
+</template>
+
+<script lang="ts">
+    import {defineComponent} from 'vue';
+    import TheHeader from '@/components/the-header.vue';
+    import TheSidebar from '@/components/the-sidebar.vue';
+
+
+    export default defineComponent({
+        name: 'app',
+        components:{
+            TheHeader,TheSidebar
+        }
+    });
+</script>
+
+<style scoped>
+
+</style>
