35880c77f894a6043eda785d9b3f044ed0189730
chuikokching <455344883@qq.com>
2022-01-12 19:15:36 +0100

    5-9-extra 添加footer 修改样式

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,23 +1,307 @@
 <template>
     <a-layout-footer style="text-align: center">
-        <b>Copyright</b> ©2022 Created by chuikokching
+
+        <div class="content">
+
+            <div class="top">
+                <div class="logo-details">
+                    <span class="logo_name"><DingtalkOutlined />HomePage</span>
+                </div>
+                <div class="media-icons">
+                    <!--<div class="top:10px" color="#ffffff">关注我</div>-->
+                    <a >关注我</a>
+                    <a href="https://github.com/chuikokching"><GithubOutlined :style="{fontSize: '300%'}"/></a>
+                    <a href="https://www.youtube.com/channel/UCRXMLjOfF93sfSfHcYH_e8w"><YoutubeOutlined :style="{fontSize: '300%'}"/></a>
+                    <a href="https://www.facebook.com/profile.php?id=100009462564053"><FacebookOutlined :style="{fontSize: '300%'}"/></a>
+                    <a href="https://space.bilibili.com/5915471">
+                        <svg t="1641985570982" class="icon" viewBox="0 0 1129 1024" version="1.1"
+                        xmlns="http://www.w3.org/2000/svg" p-id="1981" width="200" height="200">
+                            <path d="M234.909 9.656a80.468 80.468 0 0 1 68.398 0 167.374 167.374 0 0 1 41.843 30.578l160.937 140.82h115.07l160.936-140.82a168.983 168.983 0 0 1 41.843-30.578A80.468 80.468 0 0 1 930.96 76.445a80.468 80.468 0 0 1-17.703 53.914 449.818 449.818 0 0 1-35.406 32.187 232.553 232.553 0 0 1-22.531 18.508h100.585a170.593 170.593 0 0 1 118.289 53.109 171.397 171.397 0 0 1 53.914 118.288v462.693a325.897 325.897 0 0 1-4.024 70.007 178.64 178.64 0 0 1-80.468 112.656 173.007 173.007 0 0 1-92.539 25.75h-738.7a341.186 341.186 0 0 1-72.421-4.024A177.835 177.835 0 0 1 28.91 939.065a172.202 172.202 0 0 1-27.36-92.539V388.662a360.498 360.498 0 0 1 0-66.789A177.03 177.03 0 0 1 162.487 178.64h105.414c-16.899-12.07-31.383-26.555-46.672-39.43a80.468 80.468 0 0 1-25.75-65.984 80.468 80.468 0 0 1 39.43-63.57M216.4 321.873a80.468 80.468 0 0 0-63.57 57.937 108.632 108.632 0 0 0 0 30.578v380.615a80.468 80.468 0 0 0 55.523 80.469 106.218 106.218 0 0 0 34.601 5.632h654.208a80.468 80.468 0 0 0 76.444-47.476 112.656 112.656 0 0 0 8.047-53.109v-354.06a135.187 135.187 0 0 0 0-38.625 80.468 80.468 0 0 0-52.304-54.719 129.554 129.554 0 0 0-49.89-7.242H254.22a268.764 268.764 0 0 0-37.82 0z m0 0" fill="#20B0E3" p-id="1982"></path><path d="M348.369 447.404a80.468 80.468 0 0 1 55.523 18.507 80.468 80.468 0 0 1 28.164 59.547v80.468a80.468 80.468 0 0 1-16.094 51.5 80.468 80.468 0 0 1-131.968-9.656 104.609 104.609 0 0 1-10.46-54.719v-80.468a80.468 80.468 0 0 1 70.007-67.593z m416.02 0a80.468 80.468 0 0 1 86.102 75.64v80.468a94.148 94.148 0 0 1-12.07 53.11 80.468 80.468 0 0 1-132.773 0 95.757 95.757 0 0 1-12.875-57.133V519.02a80.468 80.468 0 0 1 70.007-70.812z m0 0" fill="#20B0E3" p-id="1983"></path></svg></a>
+                    <a><svg t="1642001067722" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4697" width="200" height="200"><path d="M724 458.67s-55.333-42.667-76-133.333c0 0-35.333 26-41.333 79.333 0 0-4.001 44.667 28.666 72.667 0 0 16 23.334 18 41.334 0 0-62-51.334-77.333-115.334 0 0-14.001-32.667 13.333-116.667 0 0-77.333 48-89.333 103.333 0 0-7.69 35.452 27.333 69.333 0 0-46.667-26.666-45.333-73.333 0 0-1.334-44 71.333-101.333 0 0 34-25.333 6.667-80.667 0 0-42-73.342-64-88.004 0 0 10 56-77 155 0 0-27 39-28 82 0 0-6 77 121 120 0 0 153 46 160 139 0 0 27 84-69 132 0 0-91 46-159-9 0 0-43.666-60.666 27.667-108.333 0 0-82-25.666-101 48.667 0 0-7.667 52.667 64.667 85 0 0 84 42.034 185 1.684S744.998 665.667 749.331 621c0 0 9-44-22-109 0 0 75 39.334 74 105.667 0 0 6.334 56.994-93.333 112.661S644.666 756.667 512 848.667c0 0-85-57.917-222-126.167 0 0-66.667-34.166-71.667-105.166 0 0-7-66.333 81.333-118.667 0 0-52.667 57.667-43.667 113 0 0 3.667 75.167 118.5 126.333 0 0-58.5-50.001-39.75-128.501 0 0 25.53-116.166 175.057-122.166 0 0-162.141-48.661-158.807-154.995 0 0-17 44.323-96 138.323 0 0-54 62.004-71 128.004 0 0-16 59.333 9 88.333 0 0 20 35.5 122.5 89 0 0 73 37 196.5 134.5 0 0 43.333-53.166 197.333-137.166 0 0 101.333-46 120-128.667 0.001 0.002 21.335-91.328-105.332-185.995z" p-id="4698" fill="#d81e06"></path><path d="M623.5 634c-3.5-75-66.5-97-66.5-97-118.5-36-150.5 61.5-150.5 61.5C507 553.5 545 626 545 626c28 42-5 86-5 86 82-26 83.5-78 83.5-78z" p-id="4699" fill="#d81e06"></path></svg></a>
+                </div>
+            </div>
+
+            <div class="link-boxes">
+                <ul class="box">
+                    <li class="link_name">快捷链接</li>
+                    <li><router-link to="/">首页</router-link></li>
+                    <li><router-link to="/blog">文档</router-link></li>
+                    <li><router-link to="/album">相册</router-link></li>
+                    <li><router-link to="/work">作品</router-link></li>
+                    <li><router-link to="/ebook">知识库</router-link></li>
+                    <li><router-link to="/about">关于我</router-link></li>
+                    <li><router-link to="/contact">联系我</router-link></li>
+                </ul>
+                <ul class="box">
+                    <li class="link_name">所用技术链接</li>
+                    <li><a href="https://v3.vuejs.org/guide/introduction.html">
+                        <svg t="1641985892875" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2714" width="200" height="200"><path d="M615.6 123.6h165.5L512 589.7 242.9 123.6H63.5L512 900.4l448.5-776.9z" fill="#41B883" p-id="2715"></path><path d="M781.1 123.6H615.6L512 303 408.4 123.6H242.9L512 589.7z" fill="#34495E" p-id="2716"></path></svg></a></li>
+                    <li><a href="https://2x.antdv.com/docs/vue/introduce/">
+                        <svg t="1641985992790" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3015" width="200" height="200"><path d="M716.3 313.8c19-18.9 19-49.7 0-68.6l-69.9-69.9 0.1 0.1c-18.5-18.5-50.3-50.3-95.3-95.2-21.2-20.7-55.5-20.5-76.5 0.5L80.9 474.2c-21.2 21.1-21.2 55.3 0 76.4L474.6 944c21.2 21.1 55.4 21.1 76.5 0l165.1-165c19-18.9 19-49.7 0-68.6-19-18.9-49.7-18.9-68.7 0l-125 125.2c-5.2 5.2-13.3 5.2-18.5 0L189.5 521.4c-5.2-5.2-5.2-13.3 0-18.5l314.4-314.2c0.4-0.4 0.9-0.7 1.3-1.1 5.2-4.1 12.4-3.7 17.2 1.1l125.2 125.1c19 19 49.8 19 68.7 0zM408.6 514.4a106.3 106.2 0 1 0 212.6 0 106.3 106.2 0 1 0-212.6 0z m536.2-38.6L821.9 353.5c-19-18.9-49.8-18.9-68.7 0.1-19 18.9-19 49.7 0 68.6l83 82.9c5.2 5.2 5.2 13.3 0 18.5l-81.8 81.7c-19 18.9-19 49.7 0 68.6 19 18.9 49.7 18.9 68.7 0l121.8-121.7c21.1-21.1 21.1-55.2-0.1-76.4z" p-id="3016" fill="#1296db"></path></svg></a></li>
+                    <li><a href="https://spring.io/projects/spring-boot">
+                        <svg t="1641986723891" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3456" width="200" height="200"><path d="M862.08 699.392c-105.344 140.330667-330.282667 92.970667-474.538667 99.754667 0 0-25.557333 1.450667-51.242666 5.674666 0 0 9.728-4.138667 22.144-8.448 101.290667-35.029333 149.162667-42.069333 210.730666-73.685333 115.626667-59.221333 230.741333-188.288 254.165334-322.346667-44.032 128.938667-177.92 239.914667-299.818667 284.970667-83.413333 30.805333-234.325333 60.757333-234.368 60.757333a225.28 225.28 0 0 1-6.101333-3.242666c-102.613333-49.92-105.6-272.213333 80.810666-343.850667 81.749333-31.402667 159.872-14.165333 248.234667-35.2 94.208-22.4 203.349333-93.013333 247.68-185.344 49.706667 147.541333 109.44 378.282667 2.304 520.96z m1.792-566.613333a393.045333 393.045333 0 0 1-45.44 80.64 425.898667 425.898667 0 0 0-305.792-129.322667C276.992 84.096 85.333333 275.754667 85.333333 511.36a425.941333 425.941333 0 0 0 136.746667 312.917333l9.386667 8.277334a36.522667 36.522667 0 1 1 0.042666 0.042666l6.357334 5.632A424.96 424.96 0 0 0 512.64 938.666667c225.194667 0 410.154667-175.274667 425.984-396.458667 11.690667-108.330667-20.309333-245.888-74.752-409.429333" p-id="3457" fill="#019b5a"></path></svg>
+                    </a></li>
+
+
+                </ul>
+                <ul class="box">
+                    <li class="link_name">微信好友申请</li>
+                    <li><a>Avatar</a></li>
+
+                </ul>
+            </div>
+        </div>
+
+        <!--<div class="subscription">-->
+            <!--<ul>-->
+                <!--<li><b>关注我</b></li>-->
+                <!--<li><a href="https://space.bilibili.com/5915471?spm_id_from=333.1007.0.0">Bilibili</a></li>-->
+                <!--<li><a href="https://github.com/chuikokching"><div><GithubOutlined /></div></a></li>-->
+            <!--<li><a>Facebook</a></li>-->
+            <!--<li><a>Youtube</a></li>-->
+            <!--</ul>-->
+        <!--</div>-->
+
+
     </a-layout-footer>
+
+    <div class="splitline"></div>
+    <div class="copyright">
+        <b>Copyright</b> ©2022 Created by CHUIKOKCHING.
+    </div>
 </template>
 
 <script lang="ts">
     import {defineComponent} from 'vue';
+    import {
+        createFromIconfontCN,
+        DingtalkOutlined,
+        FacebookOutlined,
+        GithubOutlined,
+        YoutubeOutlined
+    } from '@ant-design/icons-vue';
+
+
+    const IconFont = createFromIconfontCN({
+        scriptUrl: '//at.alicdn.com/t/font_8d5l8fzk5b87iudi.js',
+    });
 
     export default defineComponent({
-        name: 'the-footer'
+        name: 'the-footer',
+        components:{
+            IconFont,
+            GithubOutlined,
+            DingtalkOutlined,
+            YoutubeOutlined,
+            FacebookOutlined,
+        }
     });
 </script>
 
 <style scoped>
+
+    svg{
+        width: 2em;
+        height: 3em;
+    }
+
+    .top .media-icons a:nth-child(1){
+        background: #140B5C;
+    }
+    .top .media-icons a:nth-child(2){
+        background: #1DA1F2;
+    }
+    .top .media-icons a:nth-child(2):hover{
+        color: #1DA1F2;
+        background: #fff;
+    }
+    .top .media-icons a:nth-child(3){
+        background: #E1306C;
+    }
+    .top .media-icons a:nth-child(3):hover{
+        color: #E1306C;
+        background: #fff;
+    }
+    .top .media-icons a:nth-child(4){
+        background: #0077B5;
+    }
+    .top .media-icons a:nth-child(4):hover{
+        color: #0077B5;
+        background: #fff;
+    }
+    .top .media-icons a:nth-child(5){
+        background: #dce0e6;
+    }
+    .top .media-icons a:nth-child(5):hover{
+        color: #FF0000;
+        background: #fff;
+    }
+
+    .top .media-icons a:nth-child(6){
+        background: #ffffff;
+    }
+    .top .media-icons a:nth-child(6):hover{
+        color: #722ed1;
+        background: #ffadd2;
+    }
+
+
+
+    .content .top .media-icons a{
+        height: 39px;
+        width: 39px;
+        margin: 0 8px;
+        border-radius: 50%;
+        text-align: center;
+        line-height: 40px;
+        color: #fff;
+        font-size: 13px;
+        text-decoration: none;
+        transition: all 0.4s ease;
+    }
+
+    .content .top .logo-details{
+        color: #fff;
+        font-size: 30px;
+    }
+
+    .content .top .media-icons{
+        display: flex;
+    }
+
+    .content .top{
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        margin-bottom: 50px;
+    }
+
+
+    .link-boxes{
+        width: 100%;
+        display: flex;
+        justify-content: space-around;
+        color: #ffffff;
+    }
+
+    .link-boxes .box{
+        width: calc(100% / 5 - 10px);
+        color: #ffffff;
+    }
+
+    .link-boxes .box .link_name{
+        color: #ffffff;
+        font-size: 18px;
+        font-weight: 400;
+        margin-bottom: 10px;
+        position: relative;
+    }
+
+    .link-boxes .box .link_name::before{
+
+        position: absolute;
+        left: 0;
+        bottom: -2px;
+        height: 2px;
+        width: 35px;
+        background: #ffffff;
+    }
+
+    .link-boxes .box li{
+        margin: 6px 0;
+        list-style: none;
+    }
+
+    .link-boxes .box li a{
+        color: #ffffff;
+        font-size: 14px;
+        font-weight: 400;
+        text-decoration: none;
+        opacity: 0.8;
+        transition: all 0.4s ease
+    }
+
+    .link-boxes .box li a:hover{
+        opacity: 1;
+        text-decoration: underline;
+    }
+
+    .link-boxes{
+        margin-right: 55px;
+    }
+
+    .link-boxes .input-box input{
+        height: 40px;
+        width: calc(100% + 55px);
+        outline: none;
+        border: 2px solid #AFAFB6;
+        background: #140B5C;
+        border-radius: 4px;
+        padding: 0 15px;
+        font-size: 15px;
+        color: #fff;
+        margin-top: 5px;
+    }
+
+    .link-boxes .input-box input::placeholder{
+        color: #AFAFB6;
+        font-size: 16px;
+    }
+    .link-boxes .input-box input[type="button"]{
+        background: #fff;
+        color: #140B5C;
+        border: none;
+        font-size: 18px;
+        font-weight: 500;
+        margin: 4px 0;
+        opacity: 0.8;
+        cursor: pointer;
+        transition: all 0.4s ease;
+    }
+
+
+    /*-----*/
+
     .ant-layout-footer {
         bottom: 0;
         width: 100%;
-        background-color: #ffffff;
+        background-color: #140B5C;
         left:0;
+    }
+
+    .content {
+        width: 100%;
+        height: 400px;
+    }
+
+    .subscription{
+        font-size: 14px;
+    }
+
+    .copyright{
+        width: 100%;
+        height: 80px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #140B5C;
+        color: #ffffff;
+    }
+
+    .subscription ul{
+        display: flex;
+        align-items: center;
+        list-style-type: none;
+    }
+
+    .subscription ul li{
+        margin-left: 13px;
+    }
+
+    .splitline{
+        height: 1px;
+        width: 100%;
+        background-color: #dce0e6;
     }
 
 </style>
