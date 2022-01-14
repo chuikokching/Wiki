8ae0818c6cea6e389f149ba71ead7d59d6ef7479
chuikokching <455344883@qq.com>
2022-01-14 16:48:28 +0100

    6-4 admin-ebook开发 PageHelper实现后端分页

--- src/main/java/com/ckc/wiki/service/EbookService.java
+++ src/main/java/com/ckc/wiki/service/EbookService.java
@@ -1,12 +1,13 @@
 package com.ckc.wiki.service;
 
-import com.ckc.wiki.config.WikiApplication;
 import com.ckc.wiki.domain.Ebook;
 import com.ckc.wiki.domain.EbookExample;
 import com.ckc.wiki.mapper.EbookMapper;
 import com.ckc.wiki.req.EbookReq;
 import com.ckc.wiki.resp.EbookResp;
 import com.ckc.wiki.util.CopyUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -22,9 +23,12 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
 
     public List<EbookResp> list(EbookReq req){
+
+
+
         //不管哪张表,写法固定
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
@@ -36,8 +40,18 @@ public class EbookService {
         }
         //criteria.andDescriptionLike("%"+req.getDescription()+"%");
 
+
+        //后端分页查抄 只对第一个遇到的sql起作用(important!!!!)
+        //分页4个重要参数:
+        // @param: pageNum,pageSize,
+        // @return: ebookList,pageInfo.getTotal(总行数)
+
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList  = ebookMapper.selectByExample(ebookExample);
 
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{} ",pageInfo.getTotal());
+        LOG.info("总页数：{} ",pageInfo.getPages());
 
 //        List<EbookResp> respList = new ArrayList<>();
 //        for (Ebook ebook : ebookList) {
